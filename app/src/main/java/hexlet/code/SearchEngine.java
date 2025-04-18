54af5e6bf9ea0b6b51be77ec51cd1c3dcd3e088a
Ахатов Илья <gesnuby@gmail.com>
2025-04-18 22:25:45 +0300

    Шаг 2 - Обработка текста

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -3,15 +3,23 @@ package hexlet.code;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class SearchEngine {
 
     public static List<String> search(List<Map<String, String>> documents, String stringToSearch) {
+        var pattern = Pattern.compile("\\w+");
         var result = new ArrayList<String>();
         for (Map<String, String> document : documents) {
             var text = document.get("text");
-            for (String word : text.split(" ")) {
-                if (word.equals(stringToSearch)) {
+            for (String token : text.split(" ")) {
+                String term = pattern.matcher(token)
+                        .results()
+                        .map(MatchResult::group)
+                        .collect(Collectors.joining());
+                if (term.equals(stringToSearch)) {
                     result.add(document.get("id"));
                     break;
                 }
--- app/src/test/java/hexlet/code/SearchEngineTest.java
+++ app/src/test/java/hexlet/code/SearchEngineTest.java
@@ -29,6 +29,24 @@ class SearchEngineTest {
                 Map.of("id", "doc4", "text", doc4),
                 Map.of("id", "doc5", "text", doc5)
         );
-        assertThat(SearchEngine.search(docs, "shoot")).containsExactlyInAnyOrder("doc1", "doc2");
+        assertThat(SearchEngine.search(docs, "shoot"))
+                .containsExactlyInAnyOrder("doc1", "doc2");
+    }
+
+    @Test
+    void testSearchOnDocumentsWithSpecialSymbols() {
+        var doc1 = "I can't shoot straight unless I've had a shoot!";
+        var doc2 = "Don't shoot shoot shoot that thing at me.";
+        var doc3 = "I'm your shoot.";
+        var doc4 = "I'm your shoot_shoot.";
+
+        List<Map<String, String>> docs = List.of(
+                Map.of("id", "doc1", "text", doc1),
+                Map.of("id", "doc2", "text", doc2),
+                Map.of("id", "doc3", "text", doc3),
+                Map.of("id", "doc4", "text", doc4)
+        );
+        assertThat(SearchEngine.search(docs, "shoot"))
+                .containsExactlyInAnyOrder("doc1", "doc2", "doc3");
     }
 }
